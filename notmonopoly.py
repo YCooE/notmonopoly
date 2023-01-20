ca53cdf16c739f166abb3966d0504ba365811033
YCooE <31769542+YCooE@users.noreply.github.com>
2023-01-20 13:50:49 +1100

    Initial commit

---
+++ notmonopoly.py
@@ -0,0 +1,14 @@
+import json
+
+board_file = open('board.json')
+
+data = json.load(board_file)
+
+players = ['Peter', 'Billy', 'Charlotte', 'Sweedal']
+
+starting_money = 16
+player_money = [starting_money] * 4
+
+print(money)
+for i in data:
+    print(i)
